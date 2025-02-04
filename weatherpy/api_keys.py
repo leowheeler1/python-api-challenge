8be763edbfb2c4fc168ea4ac121fbdbcb1927c8d
Leo Wheeler <leo.wheeler01@gmail.com>
2025-02-04 12:37:50 -0600

    weatherpy stuff done

--- weatherpy/api_keys.py
+++ weatherpy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "edf367a0ebf218d9e40aa5f20dc5970f"
+weather_api_key = "ae5f28dc51c9950baf7a6699b08335b3"
 
 # Geoapify API Key
-geoapify_key = "30bf1dfb1c734a0fbed73797ef249449"
+geoapify_key = "ae5f28dc51c9950baf7a6699b08335b3"
